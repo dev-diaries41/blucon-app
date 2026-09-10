3c043f99d1266962bf58df9939b44761404e249e
d41dev <d41dev@proton.me>
2026-09-10 23:10:26 +0100

    update: exposed company names and service names for better seaarch flexibility

--- app/src/main/java/com/fpf/blucon/data/MetadataRepository.kt
+++ app/src/main/java/com/fpf/blucon/data/MetadataRepository.kt
@@ -8,6 +8,12 @@ class MetadataRepository(context: Context) {
     private val companyIdMap: Map<Int, String> = BluetoothDocsYamlParser.parseCompanyIdentifiers(context, R.raw.bluetooth_company_id)
     private val serviceUuidMap: Map<Int, String> = BluetoothDocsYamlParser.parseServiceUuids(context, R.raw.bluetooth_service_uuids)
 
+    val companyNames: Set<String>
+        get() = companyIdMap.values.toSet()
+
+    val serviceNames: Set<String>
+        get() = serviceUuidMap.values.toSet()
+
     fun getCompanyName(manufacturerId: Int?): String? = manufacturerId?.let{companyIdMap[it]}
 
     fun getServiceName(serviceId: Int?): String? = serviceId?.let{serviceUuidMap[it]}
