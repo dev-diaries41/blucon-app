535e6778c0e5c95c2e3743d4aa569f0af0233c31
d41dev <d41dev@proton.me>
2026-09-10 13:46:45 +0100

    fix: map to domain

--- app/src/main/java/com/fpf/blucon/data/scans/ScanEntryRepository.kt
+++ app/src/main/java/com/fpf/blucon/data/scans/ScanEntryRepository.kt
@@ -6,7 +6,7 @@ import com.fpf.blucon.data.mappers.toEntity
 
 class ScanEntryRepository(private val dao: ScanEntryDao) {
     suspend fun addEntries(entries: List<BTScanEntry>): List<Long> = dao.addEntries(entries.map{it.toEntity()})
-    suspend fun getEntries(scanId: Long? = null, deviceAddresses: List<String>? = null) = dao.getEntries(scanId, deviceAddresses)
+    suspend fun getEntries(scanId: Long? = null, deviceAddresses: List<String>? = null) = dao.getEntries(scanId, deviceAddresses).map{it.toDomain()}
     suspend fun getEntries(limit: Int, offset: Int, scanId: Long? = null, deviceAddress: String? = null, descending: Boolean = true) = if(descending){
             dao.getEntriesDesc(scanId, limit=limit, offset=offset)
         }else{
