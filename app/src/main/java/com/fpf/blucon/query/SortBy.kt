4da6376d03aac4b542f0eb6e94517bf174355ed3
d41dev <d41dev@proton.me>
2026-09-12 13:14:48 +0100

    update: default to descending

--- app/src/main/java/com/fpf/blucon/query/SortBy.kt
+++ app/src/main/java/com/fpf/blucon/query/SortBy.kt
@@ -2,5 +2,5 @@ package com.fpf.blucon.query
 
 sealed interface SortBy {
     val descending: Boolean
-    data class Date(override val descending: Boolean = false): SortBy
+    data class Date(override val descending: Boolean = true): SortBy
 }
