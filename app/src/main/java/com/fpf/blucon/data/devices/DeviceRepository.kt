f5ebd98da2fdb468df90ff0c57c0dcbd2a868c67
d41dev <d41dev@proton.me>
2026-09-14 16:33:01 +0100

    update: return domain instead of entity

--- app/src/main/java/com/fpf/blucon/data/devices/DeviceRepository.kt
+++ app/src/main/java/com/fpf/blucon/data/devices/DeviceRepository.kt
@@ -1,13 +1,17 @@
 package com.fpf.blucon.data.devices
 
+import com.fpf.blucon.bluetooth.device.DeviceInfo
+import com.fpf.blucon.data.mappers.toDomain
+import com.fpf.blucon.data.mappers.toEntity
+
 
 class DeviceRepository(
     private val dao: DeviceDao
 ) {
-    suspend fun insert(deviceNames: List<DeviceEntity>): List<Long> = dao.insert(deviceNames)
+    suspend fun insert(deviceNames: List<DeviceInfo>): List<Long> = dao.insert(deviceNames.map { it.toEntity() })
     suspend fun delete(ids: List<Long>) = dao.delete(ids)
-    suspend fun getByIds(ids: List<Long>): List<DeviceEntity> = dao.getByIds(ids)
-    suspend fun getByNames(names: List<String>): List<DeviceEntity> = dao.getByNames(names)
-    suspend fun get(): List<DeviceEntity> = dao.get()
-    suspend fun getPage(limit: Int, offset: Int): List<DeviceEntity> = dao.getPage(limit=limit, offset=offset)
+    suspend fun getByIds(ids: List<Long>): List<DeviceInfo> = dao.getByIds(ids).map{it.toDomain()}
+    suspend fun getByNames(names: List<String>): List<DeviceInfo> = dao.getByNames(names).map { it.toDomain() }
+    suspend fun get(): List<DeviceInfo> = dao.get().map{it.toDomain()}
+    suspend fun getPage(limit: Int, offset: Int): List<DeviceInfo> = dao.getPage(limit=limit, offset=offset).map { it.toDomain() }
 }
