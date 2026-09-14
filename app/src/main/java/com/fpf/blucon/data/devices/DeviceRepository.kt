a30953acd6a89f04ccb8190dfbd9a3aa42da5ee9
d41dev <d41dev@proton.me>
2026-09-14 20:06:06 +0100

    update: update addEntries method to insert unique devices in addition to entries

--- app/src/main/java/com/fpf/blucon/data/devices/DeviceRepository.kt
+++ app/src/main/java/com/fpf/blucon/data/devices/DeviceRepository.kt
@@ -1,6 +1,7 @@
 package com.fpf.blucon.data.devices
 
 import com.fpf.blucon.bluetooth.device.DeviceInfo
+import com.fpf.blucon.bluetooth.device.NewDeviceInfo
 import com.fpf.blucon.data.mappers.toDomain
 import com.fpf.blucon.data.mappers.toEntity
 
@@ -8,6 +9,7 @@ import com.fpf.blucon.data.mappers.toEntity
 class DeviceRepository(
     private val dao: DeviceDao
 ) {
+    suspend fun insert(deviceNames: List<NewDeviceInfo>): List<Long> = dao.insert(deviceNames.map { it.toEntity() })
     suspend fun insert(deviceNames: List<DeviceInfo>): List<Long> = dao.insert(deviceNames.map { it.toEntity() })
     suspend fun delete(ids: List<Long>) = dao.delete(ids)
     suspend fun getByIds(ids: List<Long>): List<DeviceInfo> = dao.getByIds(ids).map{it.toDomain()}
