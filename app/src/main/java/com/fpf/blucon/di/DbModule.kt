5a852002666b2f0e9eb4c140d4b0706164346d25
d41dev <d41dev@proton.me>
2026-09-14 16:48:48 +0100

    update: update db koin module

--- app/src/main/java/com/fpf/blucon/di/DbModule.kt
+++ app/src/main/java/com/fpf/blucon/di/DbModule.kt
@@ -2,6 +2,9 @@ package com.fpf.blucon.di
 
 import com.fpf.blucon.data.MetadataRepository
 import com.fpf.blucon.data.ScanDatabase
+import com.fpf.blucon.data.devices.DeviceRepository
+import com.fpf.blucon.data.devices.clusters.ClusterCrossRefRepository
+import com.fpf.blucon.data.devices.clusters.DeviceClusterRepository
 import com.fpf.blucon.data.scans.ScanEntryRepository
 import com.fpf.blucon.data.scans.ScanRepository
 import org.koin.android.ext.koin.androidApplication
@@ -13,7 +16,14 @@ val dbModule = module {
     }
     single { get<ScanDatabase>().scanDao() }
     single { get<ScanDatabase>().scanEntryDao() }
+    single { get<ScanDatabase>().deviceNameDao() }
+    single { get<ScanDatabase>().deviceClusterDao() }
+    single { get<ScanDatabase>().clusterCrossRefDao() }
+
     single { ScanRepository(get(), get()) }
     single{ MetadataRepository(get()) }
     single { ScanEntryRepository(dao = get(), metadataRepository = get()) } // TODO: rename metadataRepository
+    single{ DeviceRepository(get()) }
+    single{ DeviceClusterRepository(get()) }
+    single{ ClusterCrossRefRepository(get()) }
 }
